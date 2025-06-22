b448ba3fe3cb8bb771e71d2abf3c5fbbca582d8c
perttinen <samuli.viitala@gmail.com>
2025-06-22 13:56:31 +0300

    testing tests

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import startServer from "./backend/util/startServer";
 import db from "./backend/util/db";
 
-db.connectToDatabase().catch((e) => {
-  if (e instanceof Error) {
-    console.log(`Unable to connect database: ${e.message}`);
-  }
-});
-
-startServer();
+const connect = async () => {
+  await db.connectToDatabase();
+  startServer();
+};
+// db.connectToDatabase().catch((e) => {
+//   if (e instanceof Error) {
+//     console.log(`Unable to connect database: ${e.message}`);
+//   }
+// });
+connect();
