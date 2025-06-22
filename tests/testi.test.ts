eaccb2ac3c4ce3739bc9b4a31084ea7907927b7a
perttinen <samuli.viitala@gmail.com>
2025-06-22 13:50:55 +0300

    testing tests

--- tests/testi.test.ts
+++ tests/testi.test.ts
@@ -1,16 +1,16 @@
 import request from "supertest";
 import app from "../backend/app";
-import db from "../backend/util/db";
+// import db from "../backend/util/db";
 
 const halPw = process.env.HAL_PW;
 
-beforeAll(async () => {
-  await db.connectToDatabase().catch((e) => {
-    if (e instanceof Error) {
-      console.log(`Unable to connect database: ${e.message}`);
-    }
-  });
-});
+// beforeAll(async () => {
+//   await db.connectToDatabase().catch((e) => {
+//     if (e instanceof Error) {
+//       console.log(`Unable to connect database: ${e.message}`);
+//     }
+//   });
+// });
 
 describe("testing", () => {
   it("tese returns 200", async () => {
